1d509dc3d0016022b87e8e30106527e311e7efd5
vetlemakt <vetlemakt@gmail.com>
2026-04-29 20:41:33 +0200

    Shuffle 3

--- src/App.js
+++ src/App.js
@@ -1307,7 +1307,7 @@ function YouTubePlayer() {
         <iframe
           width="240"
           height="135"
-          src={`https://www.youtube.com/embed/videoseries?list=PLZ-7xLISie3crAStc-KmPn4Oausod43CV&index=${Math.floor(Math.random()*50)}&autoplay=0&rel=0`}
+          src="https://www.youtube.com/embed/videoseries?list=PLZ-7xLISie3crAStc-KmPn4Oausod43CV&autoplay=0&rel=0&shuffle=1"
           title="VM-musikk"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
